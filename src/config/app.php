b0e62b40d49b7d843f097ab2fdfe7a3ad3344696
Diederik Van Hoorebeke <diederik@glue.be>
2021-10-12 13:22:58 +0200

    Update version number

--- src/config/app.php
+++ src/config/app.php
@@ -3,8 +3,8 @@
 return [
     'id' => 'CraftCMS',
     'name' => 'Craft CMS',
-    'version' => '3.4.30',
-    'schemaVersion' => '3.4.11',
+    'version' => '3.4.30.1',
+    'schemaVersion' => '3.4.12',
     'minVersionRequired' => '2.6.2788',
     'basePath' => dirname(__DIR__), // Defines the @app alias
     'runtimePath' => '@storage/runtime', // Defines the @runtime alias
